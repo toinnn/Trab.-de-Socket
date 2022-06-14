8c1cca300821cae24859aa59d615c110f7f97455
__.:Kuror0:.__ <limaalyson@hotmail.com>
2022-06-14 17:07:10 -0300

    Images and Basic tests

---
+++ Server/game_script.py
@@ -0,0 +1,10 @@
+import js
+from js import document
+def main():
+    pyscript.write('msg', 'Hello world')
+    # msg = document.getElementById("msg")
+    # msg.innerHTML = 'Hello world___'
+
+main()
+
+print("Meu deus , meu senhor , me ajudaaa.. Por favor !!")
