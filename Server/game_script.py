7faa47caf446cf556603e9a750b8d84f9f678666
__.:Kuror0:.__ <limaalyson@hotmail.com>
2022-06-17 13:42:11 -0300

    update Do lado Server
    Algumas atualizações na aplicação e o servidor tá mais bem estruturado , embora faltem mais atualizações

--- Server/game_script.py
+++ Server/game_script.py
@@ -4,13 +4,14 @@ from pyodide import to_js, create_proxy
 
 pipe = document.querySelector(".pipe")
 mario = document.querySelector(".mario")
-button_jump = document.querySelector(".button_jump")
-def if_stop(pos , Pipe):
-    if pos <= 120 and float(mario.style.bottom[:-2]) <= 70 and pos >= 0 :
-        Pipe.style.animation = "none"
-        Pipe.style.left = f"{pos}px"
-        mario.src = "images//game-over.png"
-        mario.style.width = "75px"
+buttom_jump = document.querySelector(".button_jump")
+# def if_stop(pos , Pipe):
+#     if pos <= 120 and float(mario.style.bottom[:-2]) <= 70 and pos >= 0 :
+#         Pipe.style.animation = "none"
+#         Pipe.style.left = f"{pos}px"
+#         mario.src = "images//game-over.png"
+#         mario.style.width = "75px"
+#         buttom_jump.textContent = "Re-Start"
 def jump(entity):
     var =js.window.getComputedStyle( mario).bottom
     pyscript.write('msg',var[:-2])
@@ -29,6 +30,7 @@ class game():
         self.mario = document.querySelector(".mario")
         self.velocity = 0
         self.g = 50
+        self.buttom_Jump = False
 
     def gravity(self , ms = 1000) -> None :
         """ms == milisegundos entre cada step do loop de movimento """
@@ -49,10 +51,32 @@ class game():
         pyscript.write('msg',var[:-2])
         
         # mario.style.bottom = f"{float(var[:-2]) + 80}px"
-        
-        var =js.window.getComputedStyle( mario).bottom
-        pyscript.write('msg',var[:-2])
-        self.velocity = -110
+        js.console.log(f"entrou e self.buttom_Jump = {self.buttom_Jump}")
+        if self.buttom_Jump :
+            #PULA
+            var =js.window.getComputedStyle( mario).bottom
+            pyscript.write('msg',var[:-2])
+            self.velocity = -110
+        else   :
+            #INICIA UMA PARTIDA
+            self.velocity = -110
+            buttom_jump.textContent = "Jump"
+            self.buttom_Jump = True
+            mario.src = "images//mario.gif"
+            mario.style.width = "150px"
+            # pipe.style.left = "800px"
+            # pipe.style.animation = 'pipe_move 2.5s infinite linear'
+            pipe.style.animationPlayState = 'running'
+
+    def if_stop(self, pos , Pipe):
+        if pos <= 120 and float(mario.style.bottom[:-2]) <= 70 and pos >= 0 and self.buttom_Jump == True:
+            # Pipe.style.animation = "none"
+            Pipe.style.animationPlayState = 'paused'
+            # Pipe.style.left = f"{pos}px"
+            mario.src = "images//game-over.png"
+            mario.style.width = "75px"
+            buttom_jump.textContent = "Re-Start"
+            self.buttom_Jump = False
 
 
 def main():
@@ -62,14 +86,15 @@ def main():
     pipe_pos = pipe.getBoundingClientRect()
     jogo = game()
     
-    js.setInterval(to_js(lambda : if_stop(pipe.offsetLeft , pipe)) , 1 )
+    js.setInterval(to_js(lambda : jogo.if_stop(pipe.offsetLeft , pipe)) , 1 )
     jump(mario)
     jump(mario)
+    buttom_jump.textContent = "Start"
     # js.setInterval(to_js(lambda : js.console.log( f"{mario.offsetLeft }")) , 50 )
     # js.setInterval(to_js(lambda : pyscript.write("msg" ,pipe.offsetLeft )) , 50 )
 
     mario.addEventListener("click" , create_proxy(jogo.jump) )
-    button_jump.addEventListener("click" , create_proxy(jogo.jump) )
+    buttom_jump.addEventListener("click" , create_proxy(jogo.jump) )
     js.setInterval(to_js(lambda : jogo.gravity(50 )) , 50 )
     
     # pipe.style.left = "250px"
--- Server/server_Socket.py
+++ Server/server_Socket.py
@@ -1,12 +1,58 @@
 from socket import *
-# import thread
+import threading
 
-server_Name = ""
+server_Name = gethostbyname(gethostname())
 server_Port = 12000
-
+# addr = ()
+print(server_Name)
 class server_skt():
-     def __init__(self) -> None:
+    def __init__(self) -> None:
         self.skt_Server  = socket(AF_INET , SOCK_STREAM)
-        self.skt_Server.bind(("" , server_Port))
+        self.skt_Server.bind((server_Name , server_Port))
         self.skt_Server.listen(1)
         print("Server Ligado")
+    
+    def handle_request(self , connection , addr):
+        print("[NOVA Conexão..]")
+        # global conexoes
+        # name = False
+
+        while True :
+            msg = connection.recv(1024).decode('utf-8')
+            if msg :
+                cmd_request = msg.split(' ')
+                method = cmd_request[0]
+                args = cmd_request[1:]
+                print('Client request ',args)
+                try :
+                    for i in args:
+                        file = open(i,"rb")
+                        response = file.read()
+                        file.close()
+
+                        header = 'HTTP/1.1 200 OK\n'
+                        if i.endswith(".png"):
+                            header += f"Content-Type: image/png"+"\n\n"
+                        elif i.endswith(".gif") :
+                            header += f"Content-Type: image/gif"+"\n\n"
+                        elif i.endswith(".css") :
+                            header += f"Content-Type: text/css"+"\n\n"
+                        else:
+                            header += f"Content-Type: text/html"+"\n\n"
+
+                except Exception as e:
+                    header   = 'HTTP/1.1 404 Not Found\n\n'
+                    response = '<html><body><center><h3>Error 404: File not found</h3><p>Python HTTP Server</p></center></body></html>'.encode('utf-8')
+
+                final_response  = header.encode('utf-8')
+                final_response += response
+                connection.send(final_response)
+                connection.close()
+                return
+
+    def start_server(self):
+        listen()
+        print("[Server Ouvindo...]")
+        while True:
+            client = accept()
+            td = threading.Thread(target = self.handle_request , args=client)
