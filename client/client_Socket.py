9891734bdd4db4a70b9ed1bcf25443adb64768bd
__.:Kuror0:.__ <limaalyson@hotmail.com>
2022-06-20 00:47:26 -0300

    Update client_Socket.py

--- client/client_Socket.py
+++ client/client_Socket.py
@@ -1,8 +1,8 @@
 from socket import *
 # import codecs
 import webbrowser
-# import thread
 # from lxml import html
+from bs4 import BeautifulSoup as bs
 
 server_Name = gethostbyname(gethostname())
 server_Port = 12000
@@ -20,10 +20,46 @@ class client_skt() :
     def recv(self , buffer : int = 2048 ):
         return self.skt_Client.recv(buffer)
 
+    def http_request(self,server_Name , server_Port , file = "index.html"):
+        self.connect( server_Name , server_Port)
+        request = f"GET /{file} HTTP/1.1\r\nHost:{server_Name}:{server_Port}\r\nConnection: keep-alive".encode()
+        self.send(request)
+
+        response = ''
+        while True:
+            recv = client.recv(1024)
+            if not recv:
+                break
+            response += recv.decode('utf-8') 
+
+        self.skt_Client.close()
+        # print(response)
+        response = response.split("\r")
+        new_response = "<!DOCTYPE html>\n<html lang=\"en\">"
+        for i in response[2:]:
+            new_response += i
+        print(new_response)
+        f = open("response.html", "w")
+        f.write(new_response)
+        f.close()  
+        return new_response 
+
+    def http_request_all(self,server_Name , server_Port , file = "index.html") :
+        http = self.http_request(server_Name , server_Port)
+        soup = bs( http , 'html.parser')
+        img_src       = [self.http_request(server_Name , server_Port , i["src"]) for i in soup.find_all('img')]
+        py_script_src = [self.http_request(server_Name , server_Port , i["src"]) for i in soup.find_all('py-script')]
+        css_src       = [self.http_request(server_Name , server_Port , "css//style.css")]
+
+        return http
+        
+
+
 
 client = client_skt()
-client.connect( server_Name , server_Port)
-request = f"GET /index.html HTTP/1.1\r\nHost:{server_Name}:{server_Port}\r\nConnection: keep-alive".encode()
+# client.connect( server_Name , server_Port)
+client.http_request_all(server_Name , server_Port , file = "index.html")
+"""request = f"GET /index.html HTTP/1.1\r\nHost:{server_Name}:{server_Port}\r\nConnection: keep-alive".encode()
 client.send(request)
 
 response = ''
@@ -41,14 +77,23 @@ for i in response[2:]:
 print(new_response)
 f = open("response.html", "w")
 f.write(new_response)
-f.close()
+f.close()"""
+# soup = bs(new_response, 'html.parser')
+# img_src = [i["src"] for i in soup.find_all('img')]
+# py_script_src = [i["src"] for i in soup.find_all('py-script')]
+# css_src = ["css//style.css"]
+
 path = "C:/Users/limaa/PythonProjects/VsCodePython/UFC/Redes/Trab. de Socket/"
 chrome_path = "C://Program Files (x86)//Google//Chrome//Application//chrome.exe %s"
 selenium_chrome_path = "C://tools//selenium//chromedriver.exe"
 
 
 # driver.get("file://"+path+"response.html")
-print("getou")
+# print(img_src)
+# print(py_script_src)
+# print(css_src)
 
-webbrowser.get(chrome_path).open(path+"response.html")
-client.skt_Client.close()
+
+# webbrowser.get(chrome_path).open(path+"response.html")
+
+# client.skt_Client.close()
